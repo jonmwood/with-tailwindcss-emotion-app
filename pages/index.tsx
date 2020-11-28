cb1003c162b360ea48350ca4547c6a98f498305f
Jon <jonmwood@gmail.com>
2020-11-28 03:08:45 -0800

    fix css prop in emotion/react/types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import xw from 'xwind'
 import ButtonReact from '../components/ButtonReact'
 import ButtonStyled from '../components/ButtonStyled'
-import { css } from '@emotion/react'
-import { jsx } from '@emotion/react'
-// import {} from '@emotion/react/types/css-prop'
+// import { css } from '@emotion/core'
+// import { jsx } from '@emotion/core'
+import {} from '@emotion/react/types/css-prop'
 
 const Index = () => (
 	<div css={xw`grid justify-center items-center h-screen space-y-20`}>
