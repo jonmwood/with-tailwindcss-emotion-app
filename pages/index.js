dcb747ea99b81ea23f8e372cabd0389fa18a74f6
Jon <jonmwood@gmail.com>
2020-11-28 03:49:21 -0800

    added text to test build

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import ButtonReact from '../components/ButtonReact'
 import ButtonStyled from '../components/ButtonStyled'
 
 const Index = () => (
-  <div css={xw`grid justify-center items-center h-screen space-y-20`}>
-    <div css={xw`space-y-20`}>
-      <ButtonReact>@emotion/react</ButtonReact>
-      <ButtonStyled>@emotion/styled</ButtonStyled>
-    </div>
-  </div>
+	<div css={xw`grid justify-center items-center h-screen space-y-20`}>
+		<div css={xw`space-y-20`}>
+			<ButtonReact>@emotion/react test update</ButtonReact>
+			<ButtonStyled>@emotion/styled</ButtonStyled>
+		</div>
+	</div>
 )
 
 export default Index
