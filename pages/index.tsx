624231ca32ec4417a3885db20309e58b415318cc
Jon <jonmwood@gmail.com>
2020-11-28 02:25:24 -0800

    add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import xw from 'xwind'
+import ButtonReact from '../components/ButtonReact'
+import ButtonStyled from '../components/ButtonStyled'
+import { css } from '@emotion/react'
+import { jsx } from '@emotion/react'
+
+const Index = () => (
+	<div css={xw`grid justify-center items-center h-screen space-y-20`}>
+		<div css={xw`space-y-20`}>
+			<ButtonReact className="">@emotion/react</ButtonReact>
+			<ButtonStyled className="">@emotion/styled</ButtonStyled>
+		</div>
+	</div>
+)
+
+export default Index
